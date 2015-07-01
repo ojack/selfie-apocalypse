0f07c0dcb64e19c7cf69c4b831b827165f29b3b3
Olivia Jack <ojackson@gmail.com>
2015-07-01 11:32:36 -0400

    Rendered canvas into web GL

---
+++ js/AsciiGradient.js
@@ -0,0 +1,32 @@
+
+var charString = ".:-=+*#%@";
+var height = 128;
+var block = "█";
+
+var AsciiGradient = function(){
+	var canvas = document.createElement('canvas');
+	var context = canvas.getContext('2d');
+	context.font = height + 'px monospace';
+	metrics = context.measureText('i');
+
+	canvas = document.createElement('canvas');
+	canvas.height = height;
+	canvas.width = metrics.width * charString.length;
+	// canvas.height =512;
+	// canvas.width =512;
+
+	context = canvas.getContext('2d');
+	context.fillStyle = 'red';
+	context.fillRect(0,0,canvas.width,canvas.height);
+	context.font = height + 'px monospace';
+	context.fillStyle = 'black';
+
+
+	context.fillText(charString, 0, height - 1);
+
+	this.canvas = canvas;
+	//document.body.appendChild(canvas);
+
+}
+
+module.exports = AsciiGradient;
